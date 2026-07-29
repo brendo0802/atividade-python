7306231bb2edda48e99aea20b940aaac78c94add
BRENDO KIERME <brendo.kierme@senacsp.edu.br>
2026-07-29 17:07:46 -0300

    atividade django em aula

---
+++ tasks/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('P', 'pedente'),
+        ("A", "em andamento"),
+        ("C", "pedente"),
+
+    ]
+    title = models.CharField(max_length =255)
+    description = models.TextField()
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default= 'P' )
+    created_at = models.DateField(auto_now_add = True )
+    due_date = models.DateField()
+
+    def __str__(self) -> str:
+        return self.title
